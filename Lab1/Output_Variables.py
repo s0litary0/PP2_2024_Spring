fde7a917a619226f27c0503de2cc06e098bd6310
Sultanmakhmut <sultanbaibolov1@gmail.com>
2024-01-21 19:50:58 +0600

    Examples from Output Variables

---
+++ Lab1/Output_Variables.py
@@ -0,0 +1,26 @@
+#Example 1
+
+x = "Python is awesome"
+print(x)
+
+
+#Example 2
+
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+
+#Example 3
+
+x = 5
+y = 10
+print(x + y)
+
+
+#Example 4
+
+x = 5
+y = "John"
+print(x, y)
