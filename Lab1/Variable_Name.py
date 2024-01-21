4590f8c29e869eeedf11bb77d9b3f5e520bb4342
Sultanmakhmut <sultanbaibolov1@gmail.com>
2024-01-21 19:42:15 +0600

    Examples from Variable Name

---
+++ Lab1/Variable_Name.py
@@ -0,0 +1,14 @@
+#Example 1
+
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+#Example 2
+
+myVariableName = "John"
+MyVariableName = "John"
+my_variable_name = "John"
