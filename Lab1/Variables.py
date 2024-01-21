29712320a7c8f3b87458e3cbd4597d48554c6959
Sultanmakhmut <sultanbaibolov1@gmail.com>
2024-01-21 19:41:20 +0600

    Examples from Variables

---
+++ Lab1/Variables.py
@@ -0,0 +1,41 @@
+#Example 1
+
+x = 4
+y = "Sultan"
+print(x, y)
+
+
+#Example 2
+
+x = 4       # x is of type int
+x = "Samal" # x is now of type str
+print(x)
+
+
+#Example 3
+
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+
+#Example 4
+
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+
+#Example 5
+
+x = "John"
+# is the same as
+x = 'John'
+
+
+#Example 6
+
+a = 4
+A = "Sally"
+#A will not overwrite a
