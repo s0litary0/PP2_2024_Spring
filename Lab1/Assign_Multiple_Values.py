90b111f1c90221f90d70cf67cbd611386ca32395
Sultanmakhmut <sultanbaibolov1@gmail.com>
2024-01-21 19:47:02 +0600

    Examples from Assign Multiple Values

---
+++ Lab1/Assign_Multiple_Values.py
@@ -0,0 +1,15 @@
+#Example 1
+
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+
+#Example 2
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
