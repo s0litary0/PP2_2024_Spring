ddd772378bac75fc5dd15f14e7f84ff5e7ba2e4c
Sultanmakhmut <sultanbaibolov1@gmail.com>
2024-01-27 14:06:02 +0600

    Examples from Booleans

---
+++ Lab 2/Python_Booleans.py
@@ -0,0 +1,33 @@
+#Example 1
+print(1 > 0)
+print(1 == 1)
+print(1 < 0)
+
+#Example 2
+a = 5
+b = 3
+
+if a > b:
+    print("a is greater than b")
+else:
+    print("a is greater or equal b")
+
+#Example 3
+print(bool(123))
+print(bool(0), "\n")
+
+print(bool("Hello"))
+print(bool(""), "\n")
+
+#Example 4
+print(bool(["apple", "banana", "cherry"]), "\n")
+
+#Example 5
+print(bool(False),
+bool(None),
+bool(0),
+bool(""),
+bool(()),
+bool([]),
+bool({}))
+
