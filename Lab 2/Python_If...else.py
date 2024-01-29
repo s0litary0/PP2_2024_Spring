ecc5d2abb5c39ecdd25d48af260c956bd74c1a02
Sultanmakhmut <sultanbaibolov1@gmail.com>
2024-01-29 18:39:16 +0600

    Examples from If...else

---
+++ Lab 2/Python_If...else.py
@@ -0,0 +1,28 @@
+#Example 1
+
+if 2 > 1:
+    print("YES")
+else: 
+    print("2 <= 1")
+
+#Example 2
+
+a = 5
+b = 5
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+
+#Example 3
+
+if True: print("1 = 1")
+
+#Example 4
+
+print(1) if False else print(2)
+
+#Example 5
+
+if True:
+    pass
