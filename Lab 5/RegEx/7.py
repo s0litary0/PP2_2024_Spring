d0801ecde03511893736962c46150264de071e2f
Sultanmakhmut <sultanbaibolov1@gmail.com>
2024-03-05 12:38:15 +0500

    fixed

--- Lab 5/RegEx/7.py
+++ Lab 5/RegEx/7.py
@@ -2,7 +2,7 @@ import re
 
 def snake_toUpper(text):
     a = re.split('_{1}', text)
-    print(a[0] + ''.join(map(lambda x: x.title(), a)))
+    print(a[0] + ''.join(map(lambda x: x.title(), a[1:])))
 
 snake_toUpper('snake_string')
 snake_toUpper('winter_summer_holidays')
